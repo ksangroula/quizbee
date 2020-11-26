b6e43590680b46b7b23a1cc099b8b3ccbaa2668b
ksangroula <ksangroula@gmail.com>
2020-11-26 22:36:13 +0200

    quiz app using react first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./assets/style.css";
+
+class QuizBee extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="title">QuizBee</div>
+            </div>
+        );
+    }
+}
